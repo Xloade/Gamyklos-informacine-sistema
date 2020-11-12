56139d8a4cdef72af83b1a9211d06a3c425286ee
matkius <matas.ska@gmail.com>
2020-11-12 17:18:28 +0200

    vartotoju frontendas, registracija, sesijos zinutes

--- IS_Gamykla/routes/web.php
+++ IS_Gamykla/routes/web.php
@@ -17,6 +17,21 @@ Route::get('/', function () {
     return view('root');
 })->name('root.index');
 
+Route::prefix('admin')->group(function () {
+    Route::get('','AdminController@index')->name('admin.index');
+    Route::get('create','AdminController@create')->name('admin.create');
+    Route::post('store','AdminController@store')->name('admin.store');
+    Route::get('{user}','AdminController@edit')->name('admin.edit');
+    Route::get('{user}','AdminController@edit')->name('admin.edit');
+    Route::patch('{user}/update','AdminController@update')->name('admin.update');
+    Route::delete('{user}/destroy','AdminController@destroy')->name('admin.destroy');
+});
+Route::prefix('user')->group(function () {
+    Route::get('/user','UserController@editprofile')->name('user.editprofile');
+});
+
+
+
 Route::prefix('gamyklos')->group(function () {
     Route::get('', 'GamyklaController@index')->name('gamyklos.index');
 });
