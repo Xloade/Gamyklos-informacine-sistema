6fa1ca86d7db4bf3b11f89f6ec356ac040e9e8fd
Artūras <arturas.deltuvas@gmail.com>
2020-12-09 20:59:02 +0200

    migracijos

---
+++ IS_Gamykla/database/migrations/2020_12_08_210641_create_sandeliai_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSandeliaiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sandeliai', function (Blueprint $table) {
+            $table->id('sandelio_kodas');
+            $table->string('salis');
+            $table->string('miestas');
+            $table->string('gatve');
+            $table->float('talpa');
+            $table->foreignId('vadovo_id')->nullable()->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sandeliai');
+    }
+}
