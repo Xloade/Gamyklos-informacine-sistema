4d8fd87369cc9716abb24dc16fee8814eb10fd8a
Artūras <arturas.deltuvas@gmail.com>
2020-12-12 19:59:52 +0200

    seeders

--- IS_Gamykla/database/seeders/DatabaseSeeder.php
+++ IS_Gamykla/database/seeders/DatabaseSeeder.php
@@ -16,7 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
         $this->call([
             UserSeeder::class,
-            SandelisSeeder::class
+            SandelisSeeder::class,
+            PrekeSeeder::class,
+            PrekeSandelyjeSeeder::class
         ]);
     }
 }
