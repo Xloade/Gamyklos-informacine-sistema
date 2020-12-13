ac680849677c3edc55f75e066ddfe82c3c86d244
Valdas Urbonas <urbonas.val@gmail.com>
2020-12-13 15:42:47 +0200

    User fk_gamykla

--- IS_Gamykla/app/Models/User.php
+++ IS_Gamykla/app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'first_name','last_name', 'email', 'password', 'userlevel'
+        'first_name','last_name', 'email', 'password', 'userlevel','fk_gamykla'
     ];
 
     /**
